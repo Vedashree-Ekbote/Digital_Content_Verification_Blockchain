ea94d0fa99908f21dfa57d2e115dcca50091c03f
Vedashree-Ekbote <Vedashree-Ekbote>
2024-03-03 15:45:54 +0530

    updated app.js file

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const Web3 = require('web3');
 
 const app=express();
-const PORT=process.env.PORT||3000;
-app.listen(PORT,()=>{
-    console.log(`Server Listening at port:${PORT}`)
+
+app.get("/" ,(req,res)=>{
+    res.send("Hello");
 })
+const PORT=process.env.PORT||3000;
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+});
