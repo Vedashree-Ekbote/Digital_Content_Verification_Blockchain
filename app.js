957ba7c9d02f14c3619fa66b412cb233c7fc8d19
Vedashree-Ekbote <Vedashree-Ekbote>
2024-03-04 14:25:33 +0530

    added forms

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const Web3 = require('web3');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
 
-const app=express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/templates", express.static(path.join(__dirname, "templates")));
+app.use(express.static(path.join(__dirname, "templates")));
 
-app.get("/" ,(req,res)=>{
-    res.send("Hello");
-})
-const PORT=process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'Home.html')); 
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'Register.html')); 
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'CreatorLogin.html')); 
+});
+app.get("/upload_content", (req, res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'ContentUpload.html')); 
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`);
 });
